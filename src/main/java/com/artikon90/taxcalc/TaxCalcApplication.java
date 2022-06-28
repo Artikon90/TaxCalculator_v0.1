637630ae6e16ba43ca747abc6171ca70b9b6d4de
artikon90 <artikon90@gmail.com>
2022-06-29 01:43:27 +0300

    Стартовый коммит. Получилось запустить реакт с бекендом на одном порту

---
+++ src/main/java/com/artikon90/taxcalc/TaxCalcApplication.java
@@ -0,0 +1,13 @@
+package com.artikon90.taxcalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxCalcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaxCalcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/artikon90/taxcalc/TaxCalcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artikon90.taxcalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxCalcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
